8eb3322aa433a9124c1f95fc0c6d5e6279ef7942
borisngong <borisngong@gmail>
2024-05-01 15:22:41 -0700

    A new view for State objects that handles all default RESTFul API actions

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,72 +1,69 @@
 #!/usr/bin/python3
-"""This Module iss responsible for working with states"""
+"""Module for working with State objects and RESTFul API actions"""
 
 
-from flask import abort, jsonify, request
+from flask import Flask, abort, jsonify, request
 from api.v1.views import app_views
 from models import storage
 from models.state import State
 
 
+app = Flask(__name__)
+"""Ensuring the necessary imports and Flask app initialization"""
+
 @app_views.route('/states', methods=['GET'], strict_slashes=False)
 def obtain_all_states():
     """Responsible for retrieving the list of all State objects"""
     states = storage.all(State).values()
-    boro_json_reponse = [state.to_dict() for state in states]
-    return jsonify(boro_json_reponse)
+    json_response = [state.to_dict() for state in states]
+    return jsonify(json_response)
 
 
 @app_views.route('/states/<state_id>', methods=['GET'], strict_slashes=False)
 def obtain_state_by_id(state_id):
     """Responsible for retrieving a specific State object by ID"""
-    state_data = storage.get(State, state_id)
-    if state_data is None:
+    get_state = storage.get(State, state_id)
+    if get_state is None:
         abort(404)
-    boro_json_response = state_data.to_dict()
-    return jsonify(boro_json_response)
+    return jsonify(get_state.to_dict())
 
 
 @app_views.route('/states/<state_id>', methods=['DELETE'], strict_slashes=False)
-def remove_state_data(state_id):
+def remove_state(state_id):
     """Responsible for deleting a specific State object by ID"""
-    state_data = storage.get(State, state_id)
-    if state_data is None:
+    state = storage.get(State, state_id)
+    if state is None:
         abort(404)
-    storage.delete(state_data)
+    storage.delete(state)
     storage.save()
-    json_response = jsonify({}), 200
-    return json_response
+    return jsonify({}), 200
 
 
 @app_views.route('/states', methods=['POST'], strict_slashes=False)
-def make_state():
-    """Responsible for creating a new State object"""
+def create_state():
+    """Responsible for creating a new State object."""
     state_json_data = request.get_json()
     if not state_json_data:
-        abort(400, 'Not a JSON')
+        abort(400, "Not a JSON")
     if 'name' not in state_json_data:
-        abort(400, 'Missing name')
-    state = State(**state_json_data)
-    state.save()
-    boro_json_reponse = state.to_dict()
-    json_response =jsonify(boro_json_reponse), 201
-    return json_response
+        abort(400, "Missing name")
+    new_state = State(**state_json_data)
+    new_state.save()
+    return jsonify(new_state.to_dict()), 201
 
 
 @app_views.route('/states/<state_id>', methods=['PUT'], strict_slashes=False)
-def edit_update_state(state_id):
-    """Update an existing State object"""
-    state_json_data = storage.get(State, state_id)
-    if state_json_data is None:
+def update_state(state_id):
+    """Responsible for Updating an existing State object."""
+    up_state = storage.get(State, state_id)
+    if up_state is None:
         abort(404)
     data = request.get_json()
     if not data:
-        abort(400, 'Not a JSON')
-    disregard_keys = ['id', 'created_at', 'updated_at']
-    for k, v in data.items():
-        if k not in disregard_keys:
-            setattr(state_json_data, k, v)
-    state_json_data.save()
-    boro_json_response = state_json_data.to_dict()
-    json_response = jsonify(boro_json_response), 200
-    return json_response
+        abort(400, "Not a JSON")
+    ignore_keys = ['id', 'created_at', 'updated_at']
+    for key, value in data.items():
+        if key not in ignore_keys:
+            setattr(up_state, key, value)
+    up_state.save()
+    return jsonify(up_state.to_dict()), 200
